6d878ba9528c8b4072049a7b4c30a91b77617269
Leocq1 <leonardo.corales.11@gmail.com>
2022-08-09 22:26:30 -0300

    segundo commit del mismo archivo

---
+++ git.py
@@ -0,0 +1,30 @@
+"""clase de version de git para la clase de 
+
+versionado de programas
+git es un control de versiones gratuito de codigo abierto para diseñar proyectos de manera rapida.
+
+versiones posteriores y anteriores de los acrhivo.
+
+para generar puntos de control de los archivos o proectos que vamos creando para poder ir hacia delante o hacia atas 
+
+
+para creaar un punt ode control tengo archivos en carpetas como por ejejmplo 
+
+
+para crear un piunto de control (commit)
+
+tengo 3 estados 
+
+1er estado: comienzo de trabajo
+
+con git add
+2do estado (archivos listos ): agregamos las cajas listas 
+
+ con git commint
+3er estado (registro de todos los archivos)
+
+
+
+
+
+"""
